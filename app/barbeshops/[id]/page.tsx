a9d4ebfa9d02142a1a190ae5cfa6607612ab0bbd
John Costa <j.fernandes-costa@outlook.com>
2024-01-31 19:41:58 -0300

    feat: add ServiceItem Component

---
+++ app/barbeshops/[id]/page.tsx
@@ -0,0 +1,40 @@
+import { db } from "@/app/_lib/prisma";
+import BarbershopInfo from "./_components/barbeshop-info";
+import ServiceItem from "./_components/service-item";
+
+interface BarbeshopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarbeshopDetailsPage = async ({ params }: BarbeshopDetailsPageProps) => {
+  if (!params.id) {
+    //TODO: redirecionar para home page
+    return null;
+  }
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+  if (!barbershop) {
+    //TODO: redireiconar para home page
+    return null;
+  }
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+      <div className="px-5 flex flex-col gap-5 py-6">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BarbeshopDetailsPage;
